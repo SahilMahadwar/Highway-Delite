01164ad1f5e7fec42eeaf24ace26a5b1b9370e96
SahilMahadwar <accounts@mahadwar.com>
2024-06-20 22:46:05 +0530

    feat: setup tailwind

--- apps/app/src/App.tsx
+++ apps/app/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./routes/routes";
 
+import "./styles/globals.css";
+
 function App() {
   return (
     <BrowserRouter>
--- apps/app/src/pages/home.tsx
+++ apps/app/src/pages/home.tsx
@@ -1,7 +1,7 @@
 export const HomePage = () => {
   return (
     <div>
-      <h1>Home page</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 };
