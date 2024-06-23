61545aea92cc8133ba008317b9dd9580cdb6caaa
SahilMahadwar <accounts@mahadwar.com>
2024-06-23 18:48:17 +0530

    refactor: use new async handler insted of try catch

--- apps/api/src/controllers/auth.controller.ts
+++ apps/api/src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { otpTemplate } from "../libs/email-templates/otp";
+import { asyncHandler } from "../middlewares/async-handler";
 import Otp from "../models/Otp";
 import User from "../models/User";
 import { matchPassword, sendTokenResponse } from "../services/auth.service";
@@ -12,12 +13,8 @@ import { ErrorResponse } from "../utils/error-response";
 // @route   POST /api/v1/auth/login
 // @access  Public
 // @role    User
-export const login = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
+export const login = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
     const { email, password } = req.body;
 
     // Check user
@@ -38,62 +35,48 @@ export const login = async (
     }
 
     sendTokenResponse(user, 200, res);
-  } catch (error) {
-    res.status(500).json({
+  }
+);
+
+export const verifyOtp = asyncHandler(async (req: Request, res: Response) => {
+  const { email, otp } = req.body;
+
+  // Find the OTP record for the given email
+  const otpRecord = await Otp.findOne({ email: email });
+
+  if (!otpRecord) {
+    return res.status(400).json({
       success: false,
-      error:
-        error instanceof Error ? error.message : "Request failed try again",
+      message: "Invalid or expired OTP",
     });
   }
-};
 
-export const verifyOtp = async (req: Request, res: Response) => {
-  try {
-    const { email, otp } = req.body;
+  // Check if the OTP matches and has not expired
+  const currentTime = new Date();
 
-    // Find the OTP record for the given email
-    const otpRecord = await Otp.findOne({ email: email });
-
-    if (!otpRecord) {
-      return res.status(400).json({
-        success: false,
-        message: "Invalid or expired OTP",
-      });
-    }
-
-    // Check if the OTP matches and has not expired
-    const currentTime = new Date();
-
-    if (otpRecord.otp !== otp || otpRecord.expiresAt < currentTime) {
-      return res.status(400).json({
-        success: false,
-        message: "Invalid or expired OTP",
-      });
-    }
-
-    // Remove the OTP document from the database so it cant be used more than once
-    await Otp.deleteOne({ email: email });
-
-    // Generate JWT token
-    const token = jwt.sign({ email: email }, env.JWT_SECRET, {
-      expiresIn: "1d",
-    });
-
-    res.status(200).json({
-      success: true,
-      token: token,
-    });
-  } catch (error) {
-    res.status(500).json({
+  if (otpRecord.otp !== otp || otpRecord.expiresAt < currentTime) {
+    return res.status(400).json({
       success: false,
-      error:
-        error instanceof Error ? error.message : "Request failed try again",
+      message: "Invalid or expired OTP",
     });
   }
-};
 
-export const sendEmailVerificationOTP = async (req: Request, res: Response) => {
-  try {
+  // Remove the OTP document from the database so it can't be used more than once
+  await Otp.deleteOne({ email: email });
+
+  // Generate JWT token
+  const token = jwt.sign({ email: email }, env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  res.status(200).json({
+    success: true,
+    token: token,
+  });
+});
+
+export const sendEmailVerificationOTP = asyncHandler(
+  async (req: Request, res: Response) => {
     const { email } = req.body;
 
     // check if the user already exist
@@ -128,73 +111,59 @@ export const sendEmailVerificationOTP = async (req: Request, res: Response) => {
     res.status(200).json({
       message: "Otp sent",
     });
-  } catch (error) {
-    res.status(500).json({
+  }
+);
+
+export const signUp = asyncHandler(async (req: Request, res: Response) => {
+  const { name, email, password, signUpToken } = req.body;
+
+  // Check if the user already exists
+  const userCheck = await User.findOne({ email: email });
+  if (userCheck) {
+    return res.status(400).json({
       success: false,
-      error:
-        error instanceof Error ? error.message : "Request failed try again",
+      message: "User already exists",
     });
   }
-};
 
-export const signUp = async (req: Request, res: Response) => {
+  // check if email is verified
+  // Verify the token
+  let decoded;
   try {
-    const { name, email, password, signUpToken } = req.body;
-
-    // Check if the user already exists
-    const userCheck = await User.findOne({ email: email });
-    if (userCheck) {
-      return res.status(400).json({
-        success: false,
-        message: "User already exists",
-      });
-    }
-
-    // check if email is verified
-    // Verify the token
-    let decoded;
-    try {
-      decoded = jwt.verify(signUpToken, env.JWT_SECRET) as {
-        email: string;
-        exp: number;
-      };
-    } catch (err) {
-      return res.status(400).json({
-        success: false,
-        message: "Invalid or expired token",
-      });
-    }
-
-    // Check if the token email matches the request email
-    if (decoded.email !== email) {
-      return res.status(400).json({
-        success: false,
-        message: "Token email does not match request email",
-      });
-    }
-
-    // Check if the token is expired
-    const currentTime = Math.floor(Date.now() / 1000); // current time in seconds
-    if (decoded.exp < currentTime) {
-      return res.status(400).json({
-        success: false,
-        message: "Token is expired",
-      });
-    }
-
-    // Create user
-    const user = await User.create({
-      name,
-      email,
-      password,
-    });
-
-    sendTokenResponse(user, 200, res);
-  } catch (error) {
-    res.status(500).json({
+    decoded = jwt.verify(signUpToken, env.JWT_SECRET) as {
+      email: string;
+      exp: number;
+    };
+  } catch (err) {
+    return res.status(400).json({
       success: false,
-      error:
-        error instanceof Error ? error.message : "Request failed try again",
+      message: "Invalid or expired token",
     });
   }
-};
+
+  // Check if the token email matches the request email
+  if (decoded.email !== email) {
+    return res.status(400).json({
+      success: false,
+      message: "Token email does not match request email",
+    });
+  }
+
+  // Check if the token is expired
+  const currentTime = Math.floor(Date.now() / 1000); // current time in seconds
+  if (decoded.exp < currentTime) {
+    return res.status(400).json({
+      success: false,
+      message: "Token is expired",
+    });
+  }
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  sendTokenResponse(user, 200, res);
+});
