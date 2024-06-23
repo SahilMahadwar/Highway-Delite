0af28a1f680c5ee5e3ac36c00d58a4749427a9c4
SahilMahadwar <accounts@mahadwar.com>
2024-06-24 01:56:27 +0530

    feat: add dashboard layout

--- apps/app/src/routes/routes.tsx
+++ apps/app/src/routes/routes.tsx
@@ -1,12 +1,17 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
+import { DashboardLayout } from "../layouts/dashboard-layout";
 import { LoginPage } from "../pages/auth/login";
 import { HomePage } from "../pages/home";
 
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<Navigate to="/home" replace />} />
+
+      <Route element={<DashboardLayout />}>
+        <Route path="/home" element={<HomePage />} />
+      </Route>
 
       <Route path="/auth/login" element={<LoginPage />} />
 
